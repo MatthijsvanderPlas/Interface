0b9c62639d5ac42e9b53a208e6495291048f7e63
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-03-10 10:32:34 +0100

    typo

--- InterfaceDemo1/IDestroyable.cs
+++ InterfaceDemo1/IDestroyable.cs
@@ -2,7 +2,7 @@ namespace InterfaceDemo1;
 
 public interface IDestroyable
 {
-    // property to store the audio file of the destruction soud
+    // property to store the audio file of the destruction sound
     string DestructionSound { get; set; }
     
     // method to destroy an object
