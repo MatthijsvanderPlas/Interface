e71be41c805528dbf3bade2a4078a3910ccef0ce
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-03-10 09:17:45 +0100

    Initial commit

---
+++ Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Interfaces;
+
+internal abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        Ticket t1 = new Ticket(10);
+        Ticket t2 = new Ticket(15);
+
+        Console.WriteLine(t1.Equals(t2));
+    }
+}
---
+++ Interfaces/Ticket.cs
@@ -0,0 +1,17 @@
+namespace Interfaces;
+
+public class Ticket : IEquatable<Ticket>
+{
+    public int DurationHours { get; set; }
+
+    public Ticket(int durationHours)
+    {
+        DurationHours = durationHours;
+    }
+
+    public bool Equals(Ticket? other)
+    {
+        if (other == null) throw new ArgumentNullException(nameof(other));
+        return this.DurationHours == other.DurationHours;
+    }
+}
